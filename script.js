25b51ae6883f741fe09dc603c02d2708b6c6a8a6
Shaun Pennypacker <shaun@crc.local>
2023-05-24 09:03:25 -0400

    html, js, and css files from chatgpt

---
+++ script.js
@@ -0,0 +1,18 @@
+const text = 'What would Brian do?';
+let index = 0;
+
+function typeText() {
+    if (index < text.length) {
+        document.getElementById('content').innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeText, 150); // Speed of typing
+    } else {
+        setTimeout(redirectToOtherSite, 2000); // Wait for 2 seconds before redirect
+    }
+}
+
+function redirectToOtherSite() {
+    window.location.href = 'https://www.eyenetsecurity.com'; // Replace with your URL
+}
+
+window.onload = typeText;
